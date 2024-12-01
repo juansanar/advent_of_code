fee7459b2524065f1d86c8774b707e81406bc624
Juan C Sanchez-Arias <43007095+juansanar@users.noreply.github.com>
2024-12-01 15:52:40 -0800

    Create solution_01.R
    Solution to Challenge Day 01

---
+++ solution_01.R
@@ -0,0 +1,18 @@
+input <- read.table("https://raw.githubusercontent.com/juansanar/advent_of_code/refs/heads/main/input_01.txt", quote="\"", comment.char="")
+
+challenge_01 <- function(data){
+    left <- data[[1]]
+    right <- data[[2]]
+    diff_list <- sum(abs(sort((left)) - sort(right)))
+    print(diff_list)
+}
+
+challenge_02 <- function(data){
+    reps <-table(data[[2]][data[[2]] %in% data[[1]]])
+    df<-as.data.frame(reps)
+    df$Var1 <-as.numeric(as.character(df$Var1))
+    print(sum(df$Var1*df$Freq))
+}
+
+challenge_01(input)
+challenge_02(input)
